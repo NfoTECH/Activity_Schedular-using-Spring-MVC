f3ef79998c3916fb9d01aaca52b0e2561bfc7fd7
NfoTECH <fortunenwachukwu@gmail.com>
2022-08-29 16:17:17 +0100

    Packages created

---
+++ src/main/java/com/fortunate/activitytracker/model/Task.java
@@ -0,0 +1,18 @@
+package com.fortunate.activitytracker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
---
+++ src/main/java/com/fortunate/activitytracker/model/User.java
@@ -0,0 +1,26 @@
+package com.fortunate.activitytracker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
