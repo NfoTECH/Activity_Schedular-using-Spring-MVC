81493e799737e85e4709cdeb215982ab461f280d
NfoTECH <fortunenwachukwu@gmail.com>
2022-09-17 11:18:38 +0100

    exception controller class modified

--- src/main/java/com/fortunate/activitytracker/controller/ExceptionController.java
+++ src/main/java/com/fortunate/activitytracker/controller/ExceptionController.java
@@ -1,8 +1,6 @@
 package com.fortunate.activitytracker.controller;
 
 import com.fortunate.activitytracker.exception.UserNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
